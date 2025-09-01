eec34cab15ceda23de4fae627f7b45d9d8bc215d
eduarek <eduarek878@gmail.com>
2025-09-01 13:09:46 -0300

    Create Conta.py

---
+++ Conta.py
@@ -0,0 +1,26 @@
+class Conta:
+    def __init__(self, titular, numero, saldo):
+        self.titular = titular
+        self.numero = numero
+        self._saldo = saldo
+    @property
+    def saldo(self):
+            return self._saldo
+    @saldo.setter
+    def saldo(self, saldo):
+            if (saldo<0):
+                print("O saldo não pode ser negativo")
+            else:
+                self._saldo = saldo
+    def saque(self, valor):
+            if (self._saldo>=valor):
+                self._saldo -= valor
+                print("Saldo realizado com sucesso")
+            else:
+                print("Saldo insuficiente")
+
+    def deposita(self, valor):
+        self._saldo += valor
+
+    def extrato(self):
+        print("Cliente", self.titular, "Saldo Atual:", self._saldo)
